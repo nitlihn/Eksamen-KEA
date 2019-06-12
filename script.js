1d449f53aa97909bb2f9e3e942bc002eb6460764
nitlihn <nita0018@stud.kea.dk>
2019-06-12 21:22:13 +0200

    hkd

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    // Queryselector på burger menu
+    // Kalder function show menu
+    document.querySelector("#menu").addEventListener("click", openNav);
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    console.log("yo");
+    document.querySelector("#mySidenav").style.width = "250px";
+    document.querySelector("#kryds").addEventListener("click", closeNav)
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.querySelector("#mySidenav").style.width = "0";
+    document.querySelector("#menu").addEventListener("click", openNav)
 }
